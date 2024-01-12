c40fe124786ff2f98eb3cbd3350a193ae35cc1bb
Nikita <barskii.no@outlook.com>
2024-01-12 18:30:20 +0300

    Add verbose_name to models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, null=True, blank=True)
-    title_jp = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(upload_to='', null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    prev_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name="next_evolutions", verbose_name="Из кого эволюционировал")
+    title = models.CharField(max_length=200, verbose_name="Имя")
+    title_en = models.CharField(max_length=200, null=True, blank=True, verbose_name="Имя (англ.)")
+    title_jp = models.CharField(max_length=200, null=True, blank=True, verbose_name="Имя(яп.)")
+    image = models.ImageField(upload_to='', null=True, blank=True, verbose_name="Изображение")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    prev_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
+                                       related_name="next_evolutions", verbose_name="Из кого эволюционировал")
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
-    
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Покемон")
+    lat = models.FloatField(verbose_name="Координаты широты")
+    lon = models.FloatField(verbose_name="Координаты долготы")
+    appeared_at = models.DateTimeField(verbose_name="Дата появления")
+    disappeared_at = models.DateTimeField(verbose_name="Дата исчезновения")
+    level = models.IntegerField(verbose_name="Уровень")
+    health = models.IntegerField(verbose_name="Здоровье")
+    strength = models.IntegerField(verbose_name="Сила")
+    defence = models.IntegerField(verbose_name="Защита")
+    stamina = models.IntegerField(verbose_name="Выносливость")
